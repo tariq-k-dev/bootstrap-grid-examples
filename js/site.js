c69e4097aaaa6022e432e0755d84da13cb6f5ba9
tariq-k-dev <tariq.k.dev@gmail.com>
2021-07-25 22:52:31 -0700

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,15 @@
+const readMoreBtn = document.querySelector('#readMoreBtn');
+
+readMoreBtn.addEventListener('click', e => {
+  e.preventDefault();
+
+  if (readMoreBtn.innerText === 'Read More') {
+    readMoreBtn.innerText = 'Read Less';
+    const readableContent = document.querySelector('#expandable-content');
+    readableContent.style.height = 'fit-content';
+  } else {
+    readMoreBtn.innerText = 'Read More';
+    const readableContent = document.querySelector('#expandable-content');
+    readableContent.style.height = '50%';
+  }
+});
